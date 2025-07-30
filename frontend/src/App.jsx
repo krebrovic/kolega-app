b80aef52085c51a14ac07c0d6d3695c5c2afabd9
krebrovic <krebrovic@gmail.com>
2025-07-30 19:03:38 +0200

    initial push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Login from "./components/login";
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
